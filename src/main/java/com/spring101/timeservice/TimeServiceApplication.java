ec7e0005699f3b1e88204fb5f504c08892ade35e
Alexandru-Petrut Popa <Alexandru-Petrut.Popa@cision.com>
2020-07-03 17:36:17 +0300

    added winsw and tutorial steps

--- src/main/java/com/spring101/timeservice/TimeServiceApplication.java
+++ src/main/java/com/spring101/timeservice/TimeServiceApplication.java
@@ -2,12 +2,21 @@ package com.spring101.timeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.GregorianCalendar;
 
 @SpringBootApplication
+@EnableScheduling
 public class TimeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeServiceApplication.class, args);
     }
 
+    @Scheduled(fixedRate = 5000)
+    public void printDate() {
+        System.out.println("Alex's Time Service : " + new GregorianCalendar().getTime());
+    }
 }
